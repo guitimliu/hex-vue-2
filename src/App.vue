1814d473362cd514355df6b22e85f1d1bfa1b386
Gui <guitimliu@gmail.com>
2022-01-24 19:21:12 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <a href="#" class="btn btn-primary">{{ text }}</a>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: '文字'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
